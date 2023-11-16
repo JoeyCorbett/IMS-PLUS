4074c9179ef500fffa529e79891a087c88ae3160
JoeyCorbett <134228957+JoeyCorbett@users.noreply.github.com>
2023-11-15 20:30:43 -0500

    Improve UI by styling elements
    Centered Shortcuts, Increased margin between shortcuts and header, added Icon to header

--- Main Script.js
+++ Main Script.js
@@ -157,13 +157,7 @@
             // Load Font Awesome Icon
            const fontAwesomeScript = document.createElement('script');
            fontAwesomeScript.crossOrigin = 'anonymous';
-           const FA_Icon =  '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><style>svg{fill:#ffffff}</style><path d="M224 16c-' +
-               '6.7 0-10.8-2.8-15.5-6.1C201.9 5.4 194 0 176 0c-30.5 0-52 43.7-66 89.4C62.7 98.1 32 112.2 32 128c0 14.3 25 27.1 64.6 35.9c-.4 4-.6 8-.6 12.1c0 17 3.3' +
-               ' 33.2 9.3 48H45.4C38 224 32 230 32 237.4c0 1.7 .3 3.4 1 5l38.8 96.9C28.2 371.8 0 423.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29' +
-               '.7c0-58.5-28.2-110.4-71.7-143L415 242.4c.6-1.6 1-3.3 1-5c0-7.4-6-13.4-13.4-13.4H342.7c6-14.8 9.3-31 9.3-48c0-4.1-.2-8.1-.6-12.1C391 155.1 416 142.3 416' +
-               ' 128c0-15.8-30.7-29.9-78-38.6C324 43.7 302.5 0 272 0c-18 0-25.9 5.4-32.5 9.9c-4.8 3.3-8.8 6.1-15.5 6.1zm56 208H267.6c-16.5 0-31.1-10.6-36.3-26.2c-2.3-7-12' +
-               '.2-7-14.5 0c-5.2 15.6-19.9 26.2-36.3 26.2H168c-22.1 0-40-17.9-40-40V169.6c28.2 4.1 61 6.4 96 6.4s67.8-2.3 96-6.4V184c0 22.1-17.9 40-40 40zm-88 96l16 32L176' +
-               ' 480 128 288l64 32zm128-32L272 480 240 352l16-32 64-32z"/></svg>"></i>'
+           const FA_Icon =  '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M418.4 157.9c35.3-8.3 61.6-40 61.6-77.9c0-44.2-35.8-80-80-80c-43.4 0-78.7 34.5-80 77.5L136.2 151.1C121.7 136.8 101.9 128 80 128c-44.2 0-80 35.8-80 80s35.8 80 80 80c12.2 0 23.8-2.7 34.1-7.6L259.7 407.8c-2.4 7.6-3.7 15.8-3.7 24.2c0 44.2 35.8 80 80 80s80-35.8 80-80c0-27.7-14-52.1-35.4-66.4l37.8-207.7zM156.3 232.2c2.2-6.9 3.5-14.2 3.7-21.7l183.8-73.5c3.6 3.5 7.4 6.7 11.6 9.5L317.6 354.1c-5.5 1.3-10.8 3.1-15.8 5.5L156.3 232.2z"/></svg>"></i>'
 
 
             // Replace with SVG Icon Path
@@ -185,10 +179,10 @@
     function ShowAlert()
     {
         Swal.fire({
-            title: '<span style="font-size: 24px; color: white;">IMS+</span>',
+            title: '<span style="font-size: 24px; color: #ffffff; font-weight: bold; padding-right: 26px; "><i class="fa-solid fa-circle-nodes" style="margin-right: 8px;"></i>IMS+</span>',
             html: `
                <div style="text-align: left; color: white; font-family: 'Arial', sans-serif;">
-                <div style="font-size: 20px; margin-bottom: 15px;"><strong>Shortcuts</strong></div>
+                <div style="margin-top: 12px;font-size: 20px; margin-bottom: 15px; text-align: center; padding: 10px; background-color: #333; color: #fff; border-radius: 5px; box-shadow: 0 4px 8px rgba(255, 255, 255, 0.1);"><strong>Shortcuts</strong></div>
                 <div style="margin-bottom: 10px; font-size: 18px;">
                     <strong>Ctrl + Enter:</strong> Resolve/Re-Open Ticket
                 </div>
